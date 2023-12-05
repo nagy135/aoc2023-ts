cac002e839298a791132026bb549c2ffd44b9137
Viktor Nagy <viktor.nagy@365.bank>
2023-12-05 12:30:43 +0100

    feat: day 3 part 2

--- 3/index.ts
+++ 3/index.ts
@@ -1,21 +1,21 @@
 export default async function main(lines: string[]) {
-  return first(lines);
-  // return second(lines);
+  // return first(lines);
+  return second(lines);
 }
 
-type Label = {
+type Label1 = {
   x: number;
   y: number;
   number: number;
 };
 
-type Tag = {
+type Tag1 = {
   x: number;
   y: number;
 };
 function first(lines: string[]) {
-  const labels: Label[] = [];
-  const tags: Tag[] = [];
+  const labels: Label1[] = [];
+  const tags: Tag1[] = [];
   let scanner = "";
   for (let y = 0; y < lines.length; y++) {
     for (let x = 0; x < lines[y].length; x++) {
@@ -64,6 +64,88 @@ function first(lines: string[]) {
     }
   }
   return sumArr.reduce((a, b) => a + b, 0).toString();
+}
+
+type Label = {
+  x: number;
+  y: number;
+  number: number;
+  tag: Tag | null;
+};
+
+type Tag = {
+  x: number;
+  y: number;
+  char: string;
+};
+
+function second(lines: string[]) {
+  const labels: Label[] = [];
+  const tags: Tag[] = [];
+  let scanner = "";
+  for (let y = 0; y < lines.length; y++) {
+    for (let x = 0; x < lines[y].length; x++) {
+      const char = lines[y][x];
+      if (is_numeric(char)) {
+        scanner += char;
+      } else if (scanner !== "") {
+        labels.push({
+          x: x - scanner.length,
+          y,
+          number: parseInt(scanner, 10),
+          tag: null,
+        });
+        scanner = "";
+      }
+      if (!is_numeric(char) && char !== ".") {
+        tags.push({
+          x,
+          y,
+          char,
+        });
+      }
+    }
+    if (scanner !== "") {
+      labels.push({
+        x: lines[y].length - scanner.length,
+        y,
+        number: parseInt(scanner, 10),
+        tag: null,
+      });
+      scanner = "";
+    }
+  }
+
+  for (const label of labels) {
+    let found = false;
+    for (const tag of tags) {
+      if (
+        !found &&
+        label.x - 1 <= tag.x &&
+        label.x + label.number.toString().length >= tag.x &&
+        label.y - 1 <= tag.y &&
+        label.y + 1 >= tag.y
+      ) {
+        found = true;
+        label.tag = tag;
+      }
+    }
+  }
+  const ratios: number[] = [];
+  for (const tag of tags) {
+    if (tag.char === "*") {
+      const numbers: number[] = [];
+      for (const label of labels) {
+        if (label.tag === tag) {
+          numbers.push(label.number);
+        }
+      }
+      if (numbers.length == 2) {
+        ratios.push(numbers[0] * numbers[1]);
+      }
+    }
+  }
+  return ratios.reduce((a, b) => a + b, 0).toString();
 }
 
 function is_numeric(str: string) {
